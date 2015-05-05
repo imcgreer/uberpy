fix more sign errors

uberpy/bokrmcal.py
@@ -184,7 +184,8 @@ def sim_init(a_init,k_init,objs,**kwargs):
 	fixed_err = kwargs.get('sim_fixed_err',0.03)
 	np.random.seed(1)
 	simdat = {}
-	simdat['a_true'] = a_range*(0.5-np.random.random_sample(a_init.shape))
+	#simdat['a_true'] = a_range*(0.5-np.random.random_sample(a_init.shape))
+	simdat['a_true'] = a_range*np.random.random_sample(a_init.shape)
 	simdat['k_true'] = k_range*np.random.random_sample(k_init.shape)
 	if kwargs.get('sim_userefmag',False):
 		simdat['mag'] = np.array([objs[i]['refMag'][0] for i in objs])
@@ -200,9 +201,11 @@ def sim_initobject(i,obj,frames,simdat,rmcal):
 	x = frames['airmass'][obj['frameIndex']]
 	dt = frames['dt'][obj['frameIndex']]
 	dk_dt = rmcal.get_terms('dkdt',0) # using a fixed value
-	mags = simdat['mag'][i] \
-	        + simdat['a_true'][obj['nightIndex'],obj['ccdNum']-1] \
+	flatfield = 0
+	mags = simdat['mag'][i] - (
+	         simdat['a_true'][obj['nightIndex'],obj['ccdNum']-1] 
 	          - (simdat['k_true'][obj['nightIndex']] + dk_dt*dt)*x
+	           + flatfield )
 	errs = np.repeat(simdat['err'][i],len(mags))
 	mags[:] += errs*np.random.normal(size=mags.shape)
 	return CalibrationObject(mags,errs)
@@ -213,6 +216,7 @@ def sim_finish(rmcal,simdat):
 	plt.subplot2grid((2,4),(0,0),colspan=3)
 	g = np.where(~rmcal.params['k']['terms'].mask)
 	dk = (rmcal.params['k']['terms']-simdat['k_true'])[g].flatten()
+	plt.axhline(0,c='gray')
 	plt.plot(dk)
 	#plt.scatter(simdat['k_true'][g].flatten(),
 	#            (rmcal.params['k']['terms']-simdat['k_true'])[g].flatten())
@@ -223,6 +227,7 @@ def sim_finish(rmcal,simdat):
 	plt.subplot2grid((2,4),(1,0),colspan=3)
 	g = np.where(~rmcal.params['a']['terms'].mask)
 	da = (rmcal.params['a']['terms']-simdat['a_true'])[g].flatten()
+	plt.axhline(0,c='gray')
 	plt.plot(da)
 	#plt.scatter(simdat['a_true'][g].flatten(),
 	#            (rmcal.params['a']['terms']-simdat['a_true'])[g].flatten())
@@ -239,10 +244,9 @@ def sim_finish(rmcal,simdat):
 	frac_sig3 = np.sum(dm3.mask & ~dm.mask) / float(np.sum(~dm.mask))
 	mm = 1000 # millimag
 	print
-	print '<da> = ',np.median(da)
-	print '<dk> = ',np.median(dk)
-	print '     @AM=1.25 ',np.median(dk)*1.25
-	print '     @AM=1.5  ',np.median(dk)*1.5
+	print '<da> = %.1f' % (mm*np.median(da))
+	print '<dk> = %.1f' % (mm*np.median(dk)),
+	print '    @AM=2.0  %.1f' % (mm*np.median(dk)*2)
 	print
 	print '%.2f %.2f %.2f %.2f %.2f' % \
 	       (mm*dm.mean(),mm*dm.std(),mm*dm3.std(),100*frac_sig3,0.0)

uberpy/ubercal.py
@@ -88,8 +88,7 @@ class CalibrationObjectSet(object):
 		dt = self.get_obstimes(ti)
 		dk_dt = self.get_terms('dkdt',0) # using a fixed value
 		flatfield = self.get_flatfields(fi,obj.get_xy())
-		#m_cal = m_inst + a - (k + dk_dt*dt)*x + flatfield
-		m_cal = m_inst - (a - (k + dk_dt*dt)*x + flatfield)
+		m_cal = m_inst + a - (k + dk_dt*dt)*x + flatfield
 		if returnBoth:
 			return m_cal,1/np.sqrt(ivar_inst),m_inst
 		else:
@@ -197,7 +196,7 @@ def ubercal_solve(calset,**kwargs):
 		if bigmatrix:
 			i2 = i1 + nobs_i
 			_ii = np.arange(i1,i2)      # indexes into rows of A (observations)
-			b[i1:i2] = m_inst - m_mean
+			b[i1:i2] = -(m_inst - m_mean)
 			cinv[i1:i2] = ivar_inst
 			A[_ii,par_a_indx] = 1
 			A[_ii,par_k_indx] = -x
@@ -206,7 +205,7 @@ def ubercal_solve(calset,**kwargs):
 			i1 += nobs_i
 			continue
 		# b column vector (eq. 13)
-		b = (m_inst - m_mean)*ivar_inst
+		b = -(m_inst - m_mean)*ivar_inst
 		wb = np.sum(b)*w
 		np.add.at( atcinvb, par_a_indx,   b-wb    )
 		np.add.at( atcinvb, par_k_indx, -(b-wb)*x )