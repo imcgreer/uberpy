actually implement flat field fit; fix sign error in residuals

uberpy/bokrmcal.py
@@ -11,7 +11,7 @@ import fitsio
 import matplotlib.pyplot as plt
 
 #from .ubercal import CalibrationObject,CalibrationObjectSet,ubercal_solve
-from ubercal import CalibrationObject,CalibrationObjectSet,ubercal_solve,init_spline_flatfields,init_array_flatfields
+from ubercal import CalibrationObject,CalibrationObjectSet,ubercal_solve,init_flatfields
 
 nX,nY = 4096,4032
 nX2 = nX//2
@@ -231,10 +231,14 @@ class SimFlatField(object):
 			self.flatfun = self._gradientfun
 	def _gradientfun(self,coeff,x,y):
 		norm = coeff.sum(axis=-1)
-		norm[norm==0] = 1
+		if np.isscalar(norm):
+			if norm>0:
+				norm **= -1
+		else:
+			norm[norm>0] **= -1
 		return ( (coeff[...,0]*(x/float(nX)) + 
 		          coeff[...,1]*(y/float(nY)))
-		         * self.dm / norm )
+		         * self.dm * norm )
 	def __call__(self,indices,x,y):
 		coeff = self.coeffs[indices]
 		return self.flatfun(coeff,x,y)
@@ -474,12 +478,14 @@ def sim_make_residual_images(rmcal,binX=32,binY=32):
 		ffmaps.append(np.array(ffmap))
 	return np.array(ffmaps)
 
-def sim_show_residual_images(rmcal,**kwargs):
-	import matplotlib.pyplot as plt
+def _init_fov_fig():
 	cmap = plt.get_cmap('jet')
 	cmap.set_bad('gray',1.)
 	plt.figure(figsize=(14,12))
 	plt.subplots_adjust(0.04,0.04,0.99,0.99,0.1,0.1)
+
+def sim_show_residual_images(rmcal,**kwargs):
+	_init_fov_fig()
 	ffmaps = sim_make_residual_images(rmcal,**kwargs)
 	for ccdNum in range(1,5):
 		ffim = np.ma.array(ffmaps[ccdNum-1],mask=np.isnan(ffmaps[ccdNum-1]))
@@ -489,5 +495,21 @@ def sim_show_residual_images(rmcal,**kwargs):
 		plt.subplot(2,2,ccdNum)
 		plt.imshow(ffim,vmin=v1,vmax=v2,
 		           origin='lower',extent=[0,nX,0,nY],interpolation='nearest')
+		plt.colorbar()
+
+def sim_show_fake_flatfields(simdat):
+	_init_fov_fig()
+	for i in range(4):
+		plt.subplot(2,2,i+1)
+		plt.imshow(simdat['flatfield'].make_image((0,i,)),
+		           origin='lower',extent=[0,nX,0,nY])
+		plt.colorbar()
+
+def show_fit_flatfields(rmcal):
+	_init_fov_fig()
+	for i,ff in enumerate(rmcal.flatfields):
+		plt.subplot(2,2,i+1)
+		plt.imshow(ff.make_image(res=64),
+		           origin='lower',extent=[0,nX,0,nY])
 		plt.colorbar()
 

uberpy/ubercal.py
@@ -13,8 +13,8 @@ class NullFlatField(object):
 		return
 	def fit(self,*args,**kwargs):
 		return
-	def __call__(self,*args):
-		return 0.0
+	def __call__(self,x,y):
+		return np.zeros_like(x)
 
 class SplineFlatField(object):
 	def __init__(self,nx,ny):
@@ -25,15 +25,17 @@ class SplineFlatField(object):
 		self.ty = [0,ny/2,ny]
 		self.kx = 3
 		self.ky = 3
+		self.splineFit = lambda x,y: np.zeros_like(x)
 	def fit(self,x,y,f,ivar=None):
 		self.splineFit = LSQBivariateSpline(x,y,f,self.tx,self.ty,w=ivar,
 		                                    kx=self.kx,ky=self.ky)
 	def __call__(self,x,y):
-		return np.array([self.splineFit(_x,_y) for _x,_y in zip(x,y)])
+		return np.array( [ self.splineFit(_x,_y).squeeze() 
+		                            for _x,_y in zip(x,y) ] )
 	def make_image(self,res=1):
 		x = np.arange(0,self.nx,res)
 		y = np.arange(0,self.ny,res)
-		return self.splineFit(x,y)
+		return self.splineFit(x,y).transpose()
 
 class FlatFieldSet(object):
 	def __init__(self,shape):
@@ -41,6 +43,14 @@ class FlatFieldSet(object):
 		self.flatfields = []
 	def get_shape(self):
 		return self.shape
+	def __call__(self,indices,x,y):
+		rv = []
+		ii = np.ravel_multi_index(indices,self.shape)
+		for i,flat in enumerate(self.flatfields):
+			jj = np.where(ii==i)[0]
+			if len(jj)>0:
+				rv.append(flat(x[jj],y[jj]))
+		return np.concatenate(rv)
 	def __iter__(self):
 		for ff in self.flatfields:
 			yield ff
@@ -160,7 +170,7 @@ class CalibrationObjectSet(object):
 		x = self.get_airmasses(xi)
 		dt = self.get_obstimes(ti)
 		dk_dt = self.get_terms('dkdt',0) # using a fixed value
-		flatfield = self.get_flatfields(fi,obj.get_xy())
+		flatfield = self.get_flatfields(fi,*obj.get_xy())
 		m_cal = m_inst + a - (k + dk_dt*dt)*x + flatfield
 		if returnBoth:
 			return m_cal,1/np.sqrt(ivar_inst),m_inst
@@ -181,8 +191,8 @@ class CalibrationObjectSet(object):
 		return self.tVals[t_indices]
 	def get_airmasses(self,x_indices):
 		return self.airmasses[x_indices]
-	def get_flatfields(self,flat_indices,xy):
-		return 0 #self.flatfields[flat_indices](*xy)
+	def get_flatfields(self,flat_indices,x,y):
+		return self.flatfields(flat_indices,x,y)
 	def update_params(self,par):
 		i0 = 0
 		for p in ['a','k','dkdt','flat']:
@@ -204,7 +214,10 @@ class CalibrationObjectSet(object):
 				# XXX should assume masking is correct here and be cleaner?
 				jj = np.where((ii==i) & (ivar>0) & ~mag.mask)[0]
 				if len(jj) > 0:
-					resv[i].append((x[jj],y[jj],mag[jj]-obj.refMag,ivar[jj]))
+					# XXX refmag should be optional, default is just offset
+					#     to weighted mean mag
+					dmag = obj.refMag - mag[jj]
+					resv[i].append((x[jj],y[jj],dmag,ivar[jj]))
 		for i,flat in enumerate(self.flatfields):
 			resarr = np.hstack(resv[i])
 			flat.fit(*resarr)
@@ -260,7 +273,7 @@ def ubercal_solve(calset,**kwargs):
 		x = calset.get_airmasses(x_indices)
 		dk_dt = calset.get_terms('dkdt',0) # using a fixed value
 		dt = calset.get_obstimes(t_indices)
-		flatfield = calset.get_flatfields(flat_indices,obj.get_xy())
+		flatfield = calset.get_flatfields(flat_indices,*obj.get_xy())
 		# construct indices into the parameter axis
 		nobs_i = obj.get_numobs()
 		par_a_indx = calset.parameter_indices('a',a_indices)